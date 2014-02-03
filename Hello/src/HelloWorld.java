92f0fc2f5df50aff893ce8150eb43e012c595b03
Dima <dima@ua2012.org.ua>
2014-02-03 09:03:09 -0800

    Class HelloWorld added

---
+++ Hello/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	/**
+	 * @author dima 20140203
+	 * @param args
+	 *
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("HelloWorld!");
+
+		
+	}
+
+}
