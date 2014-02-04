2b83d4a6edca794a26f3c2a119cc4d15cc264d7b
Dima <dima@ua2012.org.ua>
2014-02-04 09:04:25 -0800

    beeps when window close

--- Hello/src/SWTPrimer.java
+++ Hello/src/SWTPrimer.java
@@ -13,12 +13,13 @@ public class SWTPrimer {
 	public static void main (String [] args) {
 	    Display display = new Display ();
 	    Shell shell = new Shell (display);
+	    shell.setText("Hello World!");
 	    shell.open ();
-	    while (!shell.isDisposed ()) {
-	        if (!display.readAndDispatch ()) display.sleep ();
+	    while (!shell.isDisposed ())	{
+	    	if (!display.readAndDispatch()) display.sleep ();
 	    }
+	    display.beep();		//beeps when window close
 	    display.dispose ();
+	    
 	}
-	
-
 }
