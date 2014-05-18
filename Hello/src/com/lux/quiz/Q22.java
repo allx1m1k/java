d1e2af9607d5791673bef32f5e69e0cb3090b6f0
Dima <dima@ua2012.org.ua>
2014-05-19 01:23:18 +0200

    quiz lesson 1

---
+++ Hello/src/com/lux/quiz/Q22.java
@@ -0,0 +1,18 @@
+package com.lux.quiz;
+
+public class Q22 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char a = 'a'; // line 3
+		char b = '-'; // line 4
+		char c = '1'; // line 5
+		int d = 1000; // line 6
+		System.out.println(++a + b++ * c - d); // line 7
+	}
+
+}
---
+++ Hello/src/com/lux/quiz/Q23.java
@@ -0,0 +1,17 @@
+package com.lux.quiz;
+
+public class Q23 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		long b = 20;
+		short c = 30;
+		System.out.println(++a + b++ * c);
+
+	}
+
+}
