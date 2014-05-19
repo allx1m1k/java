02f5df4e959ee2ce86eb1cdb57f42f9248ff5ffc
Dima <dima@ua2012.org.ua>
2014-05-19 21:09:34 +0200

    Lesson 2 quiz binary search

---
+++ Hello/src/com/lux/quiz/MyBinarySearch.java
@@ -0,0 +1,41 @@
+package com.lux.quiz;
+
+import java.util.Arrays;
+
+public class MyBinarySearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] values ={4,7,28,-5,12,9,11};
+		
+		Arrays.sort(values);
+		
+		System.out.println(Arrays.binarySearch(values, 10)); //выводим индекс
+		//положительный индекс - эелемент найден
+		//отрицательный индекс - элемент не найден
+		
+	//реализовать алгоритм двоичного поиска	
+	/*
+	public static int mySearch()  {
+		int myIndex
+		while (a>eps){
+			a=1.0/(i*i);
+			i++;
+			result=result+a;
+			
+			
+			return 
+		
+		}
+	}
+	*/
+	
+	
+	}
+	
+	
+	
+
+
+}
