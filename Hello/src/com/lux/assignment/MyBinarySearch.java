5d9d20e223585fa2a083a34b767acd69188a0605
Dima <dima@ua2012.org.ua>
2014-05-20 22:32:40 +0200

    initial commit. works if only index value is less than searched value

--- Hello/src/com/lux/assignment/MyBinarySearch.java
+++ Hello/src/com/lux/assignment/MyBinarySearch.java
@@ -1,41 +1,56 @@
+/**
+ * @author dpeltik
+ * 
+ *Ќайти индекс введенного пользователем числа в заданном упор€доченном массиве целых чисел, использу€ метод двоичного
+ *поиска. ≈сли число не найдено, то вывести индекс позиции в которую этот элемент мог бы быть добавлен с сохранением
+ *пор€дка сортировки.
+ *	
+ */
 package com.lux.assignment;
 
 import java.util.Arrays;
 
 public class MyBinarySearch {
 
+	
+	/*
+	  ак будем решать: будем хранить lowIndex и highIndex
+	 Ќа первой итерации lowIndex = 0 и highIndex = values.length
+	  
+	 * */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] values ={4,7,28,-5,12,9,11};
+		int[] values ={4, 7, 28, -5, 12, 9, 11,};
 		
-		Arrays.sort(values);
+		Arrays.sort(values); //упор€дочим массив
 		
-		System.out.println(Arrays.binarySearch(values, 10)); //выводим индекс
-		//положительный индекс - эелемент найден
-		//отрицательный индекс - элемент не найден
+		System.out.println(Arrays.toString(values)); //выводим массив
+		/* System.out.println(Arrays.binarySearch(values, 9)); //выводим индекс элемента который ищем
+		 * 
+		 */
 		
-	//реализовать алгоритм двоичного поиска	
-	/*
-	public static int mySearch()  {
-		int myIndex
-		while (a>eps){
-			a=1.0/(i*i);
-			i++;
-			result=result+a;
-			
-			
-			return 
+		int a = 9; //что ищем 
+		int lowIndex = 0; //индекс первого элемента
+		int highIndex = values.length - 1; //индекс последнего эл-та
+		int i = (lowIndex + highIndex) / 2 ; //счетчик
 		
-		}
-	}
-	*/
-	
-	
-	}
-	
-	
-	
-
-
+				
+		while (a != values[i]) {
+			if (a <= values[i]) { //сравним искомое число с значением элемента в центре массива --> длина деленна€ на два
+				highIndex = i; //если искомое число меньше - опустим верхнюю границу
+				
+			} 
+			else {
+				lowIndex = i; //если искомое число больше - подымем нижнюю границу
+			}			//	System.arraycopy (values, 2, newArrayOfIntegers, 0, 4); аргументы Object src, srcPos, Object dest, destPos, length		
+			//[-5, 4, 7, 9, 11, 12, 28]
+			System.arraycopy (values, lowIndex, values, i, highIndex); //скопируем сам в себ€
+			System.out.println(Arrays.toString(values)); //выведем результат
+			i = (lowIndex + highIndex) / 2 ; //делим пополам 
+			}
+							
+		System.out.println(i);	
+	}	
 }
