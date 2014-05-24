cf765fc726e1c4f5389e7289902136f230d0c115
Dima <dima@ua2012.org.ua>
2014-05-24 23:36:34 +0200

    method recBinSearch is implemented

--- Hello/src/com/lux/assignment/MyBinarySearchRecursive.java
+++ Hello/src/com/lux/assignment/MyBinarySearchRecursive.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 public class MyBinarySearchRecursive {
 
 /**
- * 	
+ * Рекурсивный метод
  * @param values
  * @param lowIndex
  * @param highIndex
@@ -20,13 +20,12 @@ public class MyBinarySearchRecursive {
  * @return
  */
 	public static int recBinSearch(int[] values, int lowIndex, int highIndex, int currentIndex, int a) {
-		//приведем array в Strung для вывод эелементов массива
-		String toPrint = Arrays.toString(values);
-				
+						
 		//если искомое значение равно элементу в массиве с индексом currentIndex то вернем этот индекс
-		if (currentIndex <=0) {
+		/*if (currentIndex <=0) {
 			return currentIndex;
 		}
+		*/
 		//если искомое значение равно тому которое в текущем элементе то распечатаем результат и вернем значение
 		if (a == values[currentIndex]) 
 		{ 
@@ -43,23 +42,25 @@ public class MyBinarySearchRecursive {
 		currentIndex = (lowIndex + highIndex) / 2 ; //счетчик		
 		//System.out.println("lowIndex is " + lowIndex + " highIndex is " + highIndex + " currentIndex is " + currentIndex);
 		
-		System.out.println("Index of value " + a + " in array of " + toPrint + " is " + currentIndex); //выведем массив)		
+		//System.out.println("Index of value " + a + " in array is " + currentIndex); //выведем массив)		
 		
 		return recBinSearch(values, lowIndex, highIndex, currentIndex, a);	
 	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		int[] values ={4, 7, 28, 0, 140, -5}; //массив
-		int a = 0; //что ищем
-		
+		//приведем array в Strung для вывод эелементов массива
+		Arrays.sort(values); //упорядочим массив
+		String toPrint = Arrays.toString(values);
+		int a = 140; //что ищем
 		int lowIndex = 0; //индекс первого элемента
 		int highIndex = values.length - 1; //индекс последнего эл-та
 		int i = (lowIndex + highIndex) / 2 ; //счетчик		
-		Arrays.sort(values); //упорядочим массив
 		
-		recBinSearch(values, lowIndex, highIndex, i, a);
+		System.out.println("Index of value " + a + " in array of " + toPrint + " is " + recBinSearch(values, lowIndex, highIndex, i, a));
 		
 		
 	}
