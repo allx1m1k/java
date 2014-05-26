e9e5fe5dd4f9bdf6a89a0493226af39293d71eb8
Dima <dima@ua2012.org.ua>
2014-05-27 00:17:05 +0200

    while should be implemented

--- Hello/src/com/lux/assignment/MyBinarySort.java
+++ Hello/src/com/lux/assignment/MyBinarySort.java
@@ -5,33 +5,53 @@ import java.util.Arrays;
 public class MyBinarySort {
 
 
-	
+	/**
+	 * Метод быстрого поиска
+	 * @param massiv
+	 * @param middle
+	 * 
+	 * @return
+	 */
+	public static int[] fastSearch(int[] massiv) {
+		//int lowIndex = 0; //индекс первого элемента
+		int p = (0 + massiv.length) / 2; //опорный элемент
+		
+		
+		// будем двигать указатель от начала по одному элементу до опорного элемента пока
+		for (int i = 0 ; i <= p; i++) {
+			//будем двигать указатель с конца до опорного элемента 
+			for (int j = massiv.length - 1; j >= p; j--) {
+				//повторяем пока указатели не стали равны друг другу
+				if (i == j) {
+					return massiv;
+				}
+				//проверим элементы в левой и правой части массива
+				if (massiv[i] >= massiv[j]) {
+					//сохраним для обмена значение элеента из первого цикла
+					int temp = massiv[i];
+					//присвоим i-тому элементу значение элемента j.
+					massiv[i] = massiv[j];
+					massiv[j] = temp;
+				}
+
+			}
+		}
+	return massiv;
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	int[] values ={4, 7, 28, 0, 140, -5, 12, 9, 11, 0, -9, 1, 99, 100, 54, 36, 38 };
+	int[] values ={4, 7, 28, 140, -5, 12, 9, 11, 0, -9, 1, 99, 100, 54, 36, 38 };
 		
-		Arrays.sort(values); //упорядочим массив
-		/* System.out.println(Arrays.binarySearch(values, 9)); //выводим индекс элемента который ищем
-		 * 
-		 */
-		
-		int a = 7; //что ищем 
+		//Arrays.sort(values); //упорядочим массив
 		int lowIndex = 0; //индекс первого элемента
 		int highIndex = values.length - 1; //индекс последнего эл-та
-		int i = (lowIndex + highIndex) / 2 ; //счетчик
-		
-		//установим счетчик в исходное положение 		
-		if (a < values[i]) { //сравним искомое число с значением элемента в центре массива --> длина деленная на два
-			highIndex = i; //если искомое число меньше - опустим верхнюю границу
-		} 
-		else {
-			lowIndex = i; //если искомое число больше - подымем нижнюю границу
-		}
-		
-		
-		
-		
+		int p = (lowIndex + highIndex) / 2 ; //опорный элемент
+		//
+
+		//Arrays.toString(values)
+		System.out.println(Arrays.toString(fastSearch(values)));
 		/**
 		while (a != values[i]) {
 			//	System.arraycopy (values, 2, newArrayOfIntegers, 0, 4); аргументы Object src, srcPos, Object dest, destPos, length		
