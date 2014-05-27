a621d93742a62af4ed4a7c109e9d2cf9d88f9eb8
Dima <dima@ua2012.org.ua>
2014-05-27 20:41:23 +0200

    still not working properly

--- Hello/src/com/lux/assignment/MyBinarySort.java
+++ Hello/src/com/lux/assignment/MyBinarySort.java
@@ -12,30 +12,35 @@ public class MyBinarySort {
 	 * 
 	 * @return
 	 */
-	public static int[] fastSearch(int[] massiv) {
+	public static int[] fastSearch(int[] massiv, int p) {
 		//int lowIndex = 0; //индекс первого элемента
-		int p = (0 + massiv.length) / 2; //опорный элемент
-		
 		
 		// будем двигать указатель от начала по одному элементу до опорного элемента пока
 		for (int i = 0 ; i <= p; i++) {
 			//будем двигать указатель с конца до опорного элемента 
-			for (int j = massiv.length - 1; j >= p; j--) {
+			for (int j = massiv.length - 1 ; j >= p ; j--) {
 				//повторяем пока указатели не стали равны друг другу
 				if (i == j) {
 					return massiv;
 				}
-				//проверим элементы в левой и правой части массива
-				if (massiv[i] >= massiv[j]) {
-					//сохраним для обмена значение элеента из первого цикла
-					int temp = massiv[i];
-					//присвоим i-тому элементу значение элемента j.
-					massiv[i] = massiv[j];
-					massiv[j] = temp;
+				else {
+				//проверим элементы в левой и правой части массива				
+					if (massiv[i] >= massiv[j]) {
+						//сохраним для обмена значение элеента из первого цикла
+						int temp = massiv[i];
+						//присвоим i-тому элементу значение элемента j.
+						massiv[i] = massiv[j];
+						massiv[j] = temp;
+						//изменим опорный элемент
+						p = (i + j) / 2;						
+						//рекурсивно вызовем метод
+						return fastSearch(massiv, p);
+					}
 				}
-
 			}
+	
 		}
+		
 	return massiv;
 		
 	}
@@ -50,24 +55,9 @@ public class MyBinarySort {
 		int p = (lowIndex + highIndex) / 2 ; //опорный элемент
 		//
 
-		//Arrays.toString(values)
-		System.out.println(Arrays.toString(fastSearch(values)));
-		/**
-		while (a != values[i]) {
-			//	System.arraycopy (values, 2, newArrayOfIntegers, 0, 4); аргументы Object src, srcPos, Object dest, destPos, length		
-			//[-5, 4, 7, 9, 11, 12, 28]
-			//System.arraycopy (values, lowIndex, values, i, highIndex); //скопируем сам в себя
-			if (a < values[i]) {
-				highIndex--;//уменьшим верхнюю границу на 1
-			}
-			else  { 
-				lowIndex++; 
-			}
-			i = (lowIndex + highIndex) / 2 ; //счетчик;		
-		}
-		System.out.println(Arrays.toString(values)); //выведем массив
-		System.out.println(i);	//выведем результат
-	**/
+		System.out.println("Unsorted array is " + Arrays.toString(values));
+		System.out.println("Sorted array is " + Arrays.toString(fastSearch(values, p)));
+
 	}
 
 }
