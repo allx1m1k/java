372e93171c51e4d60c4d3a7d17b9532e689f27ad
Dima <dima@ua2012.org.ua>
2014-06-03 22:41:40 +0200

    Idea IDE fisrt class

---
+++ Hello/src/com/lux/assignment/IdeaHello.java
@@ -0,0 +1,13 @@
+package com.lux.assignment;
+
+/**
+ * Created by dima on 03.06.2014.
+ * TEST from DDDD
+ */
+public class IdeaHello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Idea community edition!");
+
+    }
+}
