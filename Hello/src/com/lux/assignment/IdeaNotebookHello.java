55712f84f17add7968dd568ed6225b419d3bcca1
Dima <dima@ua2012.org.ua>
2014-06-05 00:16:31 +0200

    commit from Idea Notebook

---
+++ Hello/src/com/lux/assignment/IdeaNotebookHello.java
@@ -0,0 +1,11 @@
+package com.lux.assignment;
+
+/**
+ * Created by dima on 6/5/2014.
+ */
+public class IdeaNotebookHello {
+
+    public static void main(String[] args) {
+        System.out.println("Hi from Idea notebool 05.06.2014");
+    }
+}
