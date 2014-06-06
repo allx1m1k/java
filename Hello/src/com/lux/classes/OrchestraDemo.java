997288b9d1456c9c1b4ca9db7a30bf0826061255
Dima <dima@ua2012.org.ua>
2014-06-06 20:30:08 +0200

    fixed

--- Hello/src/com/lux/classes/OrchestraDemo.java
+++ Hello/src/com/lux/classes/OrchestraDemo.java
@@ -7,9 +7,14 @@ public class OrchestraDemo {
     public static void main(String[] args) {
         //создадим эк-р орестра
         Orchestra orchestra = new Orchestra(4);
+        
+        orchestra.addInstrument(new Singer("Petr","Petrov", false));
         orchestra.addInstrument(new Violine("n1","red"));
         orchestra.addInstrument(new Drum("n2","green"));
         orchestra.addInstrument(new Singer("Ivan","dddd", false));
+        
+        //orchestra.addInstrument(new Singer("Petr","Petrov", false));
+        //orchestra.addInstrument(new Drum("n3","green"));
         //как MusicInstrum так и People умеют играть
         orchestra.play();
     }
