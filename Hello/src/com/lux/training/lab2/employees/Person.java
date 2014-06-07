f9a04f7a1ba7941f1311dfd0e18648df7cfb9cb3
Dima <dima@ua2012.org.ua>
2014-06-07 20:01:45 +0200

    Class Person in com.lux.training.lab2.employees

---
+++ Hello/src/com/lux/training/lab2/employees/Person.java
@@ -0,0 +1,14 @@
+package com.lux.training.lab2.employees;
+
+/**
+ * Created by dima on 07.06.2014.
+ */
+public class Person {
+    private String name;
+    private String surName;
+    private enum Dept {Finance, IT, Organizational};
+    private enum Position {Head, Manager, Senior, Junior};
+
+
+
+}
