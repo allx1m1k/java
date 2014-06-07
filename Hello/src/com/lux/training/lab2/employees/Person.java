218906ef7fd3d7fc419d25a96b310299544e1f7d
Dima <dima@ua2012.org.ua>
2014-06-07 20:16:22 +0200

    private Date birthday added

--- Hello/src/com/lux/training/lab2/employees/Person.java
+++ Hello/src/com/lux/training/lab2/employees/Person.java
@@ -1,11 +1,14 @@
 package com.lux.training.lab2.employees;
 
+import com.lux.training.utils.Date;
+
 /**
  * Created by dima on 07.06.2014.
  */
 public class Person {
     private String name;
     private String surName;
+    private Date birthday;
     private enum Dept {Finance, IT, Organizational};
     private enum Position {Head, Manager, Senior, Junior};
 
