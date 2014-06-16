e0644fdce87740741f14251e9477ba56c07d2791
Dima <dima@ua2012.org.ua>
2014-06-16 17:02:26 +0200

    SalaryMixedEmployee class implemented

---
+++ Hello/src/com/lux/training/lab2/employees/SalaryMixedEmployee.java
@@ -0,0 +1,10 @@
+package com.lux.training.lab2.employees;
+
+/**
+ * Created by dima on 16.06.2014.
+ */
+public class SalaryMixedEmployee extends ComissionEmployee {
+    private double baseSalary;
+
+
+}
