ea1b2f734dd3e0a176eb35fcdcaed9b3ae8069cd
Dima <dima@ua2012.org.ua>
2014-06-19 16:59:35 +0200

    nothing!

--- Hello/src/com/lux/assignment/MyQuickSortObjects.java
+++ Hello/src/com/lux/assignment/MyQuickSortObjects.java
@@ -2,11 +2,23 @@ package com.lux.assignment;
 
 /**
  * Created by dima on 19.06.2014.
+ * http://echuprina.blogspot.com/2012/02/comparable-comparator.html - Сортировка и упорядочивание: Comparable
  */
 public class MyQuickSortObjects implements Comparable {
+/**
+    //массив
+    private Object[] members;
+    //счетчик кол-ва отрудников в ведомости
+    private int cnt;
+ **/
+
 
     @Override
-    public int compareTo(Object o) {
-        return 0;
+    public int compareTo(Object obj) {
+
+    MyQuickSortObjects entry = (MyQuickSortObjects) obj;
+
+
+    return 0;
     }
 }
