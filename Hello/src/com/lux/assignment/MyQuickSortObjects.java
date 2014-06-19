77cd1c75bfab5a2803908102475428714b090120
Dima <dima@ua2012.org.ua>
2014-06-19 14:12:23 +0200

    MyQuickObjects class initial commit

---
+++ Hello/src/com/lux/assignment/MyQuickSortObjects.java
@@ -0,0 +1,12 @@
+package com.lux.assignment;
+
+/**
+ * Created by dima on 19.06.2014.
+ */
+public class MyQuickSortObjects implements Comparable {
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
