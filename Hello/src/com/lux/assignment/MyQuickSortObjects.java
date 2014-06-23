2b48cd9f19a509ded3f6520525dd6a2c892da01a
Dima <dima@ua2012.org.ua>
2014-06-23 23:14:19 +0200

    quick sort for Date1 is implemented using interface Comparable

--- Hello/src/com/lux/assignment/MyQuickSortObjects.java
+++ Hello/src/com/lux/assignment/MyQuickSortObjects.java
@@ -22,11 +22,11 @@ public class MyQuickSortObjects {
         while (i < j) {
             //while ((i < middle) && (massiv[i] <= massiv[middle]))
 
-            while ((i < middle) && (massiv[i].compareTo(massiv[middle]) > 0 )) i++; //перемещаем бегунок с левого края до середины проверяя значения элементов и индекс бегунка
+            while ((i < middle) && (massiv[i].compareTo(massiv[middle]) < 0 )) i++; //перемещаем бегунок с левого края до середины проверяя значения элементов и индекс бегунка
 
             //while ((j > middle) && (massiv[j] >= massiv[middle]))
 
-            while ((j > middle) && (massiv[j].compareTo(massiv[middle]) < 0 )) j--; //перемещаем бегунок с правого края до середины проверяя значения элементов и индекс бегунка
+            while ((j > middle) && (massiv[j].compareTo(massiv[middle]) > 0 )) j--; //перемещаем бегунок с правого края до середины проверяя значения элементов и индекс бегунка
 
         if (i < j) {
             Date1 temp = massiv[i];
@@ -48,16 +48,17 @@ public class MyQuickSortObjects {
     public static void main(String[] args) {
         //создадим массив с объектами типа Date1
         Date1[] myDates = new Date1[]{
-                new Date1(17, 12, 1957),
+        		new Date1(2, 10, 1991),
+        		new Date1(17, 12, 1957),
         		new Date1(7, 8, 1976),
                 new Date1(4, 9, 2010),
                 new Date1(29, 1, 1980),
-                new Date1(12, 6, 1999)
+                new Date1(12, 6, 1999),
+                new Date1(1, 6, 2014)
         };
 
         int lowIndex = 0; //индекс первого элемента
-        int highIndex = myDates.length - 1; //индекс последнего эл-та
-        int a; //temp
+        int highIndex = myDates.length - 1; //индекс последнего эл-та        
 
         /**
         System.out.println("Unsorted array is " + Arrays.toString(myDates));
@@ -68,10 +69,14 @@ public class MyQuickSortObjects {
         System.out.println("Unsorted array below: ");
         for (Date1 dates: myDates) {
             dates.printDate();
-        }
+        };
         
+        MyQuickSortObjects.objectsQuickSort(myDates, lowIndex, highIndex);
 
-
+        System.out.println("Sorted array below: ");
+        for (Date1 dates: myDates) {
+            dates.printDate();
+        };
         //System.out.println("Sorted array is " + Arrays.toString(quickSort(values, lowIndex, highIndex)));
     }
 
