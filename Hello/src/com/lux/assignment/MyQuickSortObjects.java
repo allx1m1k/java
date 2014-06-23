1666f4c92b5299af50a5e6fd40615b56cccea4dd
Dima <dima@ua2012.org.ua>
2014-06-23 21:55:07 +0200

    commit after svn crush

--- Hello/src/com/lux/assignment/MyQuickSortObjects.java
+++ Hello/src/com/lux/assignment/MyQuickSortObjects.java
@@ -1,4 +1,5 @@
 package com.lux.assignment;
+import java.util.Arrays;
 
 /**
  * Created by dima on 19.06.2014.
@@ -11,6 +12,60 @@ public class MyQuickSortObjects {
     //счетчик кол-ва отрудников в ведомости
     private int cnt;
  **/
+    public static Date1[] objectsQuickSort(Date1[] massiv, int start, int end) {
+        if (start >= end) return massiv; //прекратим выполнение если индекс с левой стороны больше либо равен индексу с правой стороны
+        int i = start;
+        int j = end;
 
+        int middle = (i + j)/2;
+
+        while (i < j) {
+            //while ((i < middle) && (massiv[i] <= massiv[middle]))
+
+            while ((i < middle) && (massiv[i].compareTo(massiv[middle]) > 0 )) i++; //перемещаем бегунок с левого края до середины проверяя значения элементов и индекс бегунка
+
+            //while ((j > middle) && (massiv[j] >= massiv[middle]))
+
+            while ((j > middle) && (massiv[j].compareTo(massiv[middle]) < 0 )) j--; //перемещаем бегунок с правого края до середины проверяя значения элементов и индекс бегунка
+
+        if (i < j) {
+            Date1 temp = massiv[i];
+            massiv[i] = massiv[j];
+            massiv[j] = temp;
+            if (i == middle) middle = j;
+            else if (j == middle) middle = i;
+
+        }
+    }
+
+    objectsQuickSort(massiv, start, middle);
+    objectsQuickSort(massiv, middle + 1, end);
+
+    return massiv;
+
+    }
+
+    public static void main(String[] args) {
+        //создадим массив с объектами типа Date1
+        Date1[] myDates = new Date1[]{
+                new Date1(7, 8, 1976),
+                new Date1(4, 9, 2010),
+                new Date1(29, 1, 1980)
+        };
+
+        int lowIndex = 0; //индекс первого элемента
+        int highIndex = myDates.length - 1; //индекс последнего эл-та
+        int a; //temp
+
+
+        System.out.println("Unsorted array is " + Arrays.toString(myDates));
+
+        for (Date1 dates: myDates) {
+            myDates[dates].printDate();
+        }
+
+
+        //System.out.println("Sorted array is " + Arrays.toString(quickSort(values, lowIndex, highIndex)));
+    }
 
 }
