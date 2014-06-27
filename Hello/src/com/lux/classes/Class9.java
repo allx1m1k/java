0362be510e89f0b2b3e029edb4f06c4f3bc4f037
Dima <dima@ua2012.org.ua>
2014-06-27 17:18:18 +0200

    Start learning catch..try

---
+++ Hello/src/com/lux/classes/Class9.java
@@ -0,0 +1,12 @@
+package com.lux.classes;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * Created by dima on 27.06.2014.
+ */
+public class Class9 {
+    InputStream is = new FileInputStream("D:\\PUB\\GoogleDrive\\JAVA\\test.txt");
+
+}
