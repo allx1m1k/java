a7e74829d4dabb3809275f883f3b4f7e4676e565
Dima <dima@ua2012.org.ua>
2014-07-03 00:18:59 +0200

    getDate() demonstrate how Class can be immutable and non immutable

--- Hello/src/com/lux/classes/Class8Final.java
+++ Hello/src/com/lux/classes/Class8Final.java
@@ -1,21 +1,32 @@
 package com.lux.classes;
 
+import com.lux.assignment.Date1;
+
 import java.util.Date;
 
 /**
  * Created by dima on 02.07.2014.
  */
 final class Class8Final {
-    private Date birthday;
+    private Date1 birthday;
 
+    /**
+     * конструктор инициализирующий атрибут birthday нашего класса в виде экземпляра класса Date1
+      */
     public Class8Final() {
-        this.birthday = new Date(1970);
-    }
-    public Date getDate() {
-        return birthday;
+        this.birthday = new Date1(31,12,1970);
     }
 
-    public Date setNewDate(Date aDate) {
+    /**
+     * метод возвращающий ссыдку на атрибут birthday нашего класса
+     * @return Date1
+     */
+    public Date1 getDate() {
+        return new Date1(birthday); //возвращаем ссылку на объект Date1 через копирующий конструктор  - класс immutable
+        //return birthday; //возвращаем ссылку на объект Date1 кот. является атрибутом в данном классе - класс не immutable
+    }
+
+    public Date1 setNewDate(Date1 aDate) {
         this.birthday = aDate;
         return birthday;
     }
@@ -23,7 +34,12 @@ final class Class8Final {
 
     public static void main(String[] args) {
         Class8Final d1 = new Class8Final(); //
-        System.out.println(d1.getDate()); //вернет ссылку на объект Date
+        System.out.println(d1.getDate()); //вернет ссылку на объект Date1 которая хранится в атрибуте birthday
+        d1.getDate().printDate(); //выведем в консоль что хранится в атрибуте birthday
+        d1.getDate().setDate(1,1,1971); //строка демонстрирующая что после применения копирующего констр-ра в методе getDate(),
+        //кажущиеся изменение setDate на самом деле наш объект d1 не изменили - применен копирующий конструктор и класс
+        //стал immutable
+        d1.getDate().printDate();
         Class8Final d2 = new Class8Final(); //
     }
 }
