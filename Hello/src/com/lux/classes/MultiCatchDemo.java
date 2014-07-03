db2f5624de5451acf5ec588a880687f288f0756d
Dima <dima@ua2012.org.ua>
2014-07-03 19:41:05 +0200

    MultiCatchDemo is demonstrate the multi try - catch block for Java 7.0

---
+++ Hello/src/com/lux/classes/MultiCatchDemo.java
@@ -0,0 +1,34 @@
+package com.lux.classes;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class MultiCatchDemo {
+
+	public static void main(String[] args) {
+
+		//Method 1
+		try { 
+			Class<?> stringClass = Class.forName("java.lang.String"); 
+			FileInputStream in = new FileInputStream("test.log") ;  
+			in.read(); 
+
+		} catch (IOException e) { 
+			System.out.println("There was an IOException "+e); 
+		} catch (ClassNotFoundException e) { 
+			System.out.println("There was a ClassCastException "+e); 
+		}
+		
+		
+		//Method 2 (Java 7 and later)
+		try { 
+			Class<?> stringClass = Class.forName("java.lang.String"); 
+			FileInputStream in = new FileInputStream("test.log") ;  
+			in.read(); 
+        } catch (IOException | ClassNotFoundException e) { 
+            System.out.println("An exception of type "+e.getClass()+" was thrown! "+e); 
+        }		
+
+	}
+
+}
