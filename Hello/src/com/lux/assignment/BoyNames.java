de72048d55c8f307119fef8fb8a6a70341a2bbcc
Dima <dima@ua2012.org.ua>
2014-07-07 23:34:51 +0200

    Class BoyNames

---
+++ Hello/src/com/lux/assignment/BoyNames.java
@@ -0,0 +1,26 @@
+package com.lux.assignment;
+
+/**
+ * В качестве 2d Array можно использовать Map http://stackoverflow.com/questions/10063354/two-dimensional-array-of-different-types
+ * Created by dima on 7/8/2014.
+ */
+public class BoyNames {
+    private String name;
+
+    public BoyNames(String name) {
+        this.name = name;
+    }
+
+    public BoyNames() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
