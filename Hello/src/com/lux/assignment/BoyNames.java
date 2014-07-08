9df5a86a84ea3ab967ee29082e38212538fe496a
Dima <dima@ua2012.org.ua>
2014-07-08 18:25:33 +0200

    nothing works properly

--- Hello/src/com/lux/assignment/BoyNames.java
+++ Hello/src/com/lux/assignment/BoyNames.java
@@ -1,26 +1,65 @@
 package com.lux.assignment;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * В качестве 2d Array можно использовать Map http://stackoverflow.com/questions/10063354/two-dimensional-array-of-different-types
  * Created by dima on 7/8/2014.
  */
 public class BoyNames {
-    private String name;
+    //чтобы хранить список имен
+    private String names[];
+    //счетчик кол-ва имен в списке
+    private int cnt; //количество имен в списке
 
-    public BoyNames(String name) {
-        this.name = name;
+    //конструктор передаем кол-во имен в списке
+    public BoyNames(int numCount) {
+        names = new String[numCount];
+        cnt = 0;
     }
 
-    public BoyNames() {
+    //метод добавления персоны
+    public void addBoy(String aName){
+        //numbers[cnt++] = instrument;
+        names[cnt++] = aName;
     }
 
-    public String getName() {
-        return name;
+    public Object[] getNames() {
+        return names;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getCnt() {
+        return cnt;
     }
 
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
+
+    public void readNamesFromFile(String path){
+        String line = null; //строка из файла
+        int i = 0;
+
+        //try - this block
+        try (
+                BufferedReader br = new BufferedReader(new FileReader(path));
+
+        ) {
+            //String line;
+            //считываем построчно пока не достигли последней строки
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                //names[i] = line.split("[ \t]");
+                names = line.split(" ");
+                i++;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
 
--- Hello/src/com/lux/assignment/BoyNamesDemo.java
+++ Hello/src/com/lux/assignment/BoyNamesDemo.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 
+
 /**
  * Created by dima on 7/8/2014.
  * How to split string http://stackoverflow.com/questions/3481828/how-to-split-a-string-in-java
@@ -13,11 +14,18 @@ import java.util.Arrays;
 public class BoyNamesDemo {
 
     public static void main(String[] args)  {
-        String[] names = new String[4];
+        Object[]names = new Object[5];
         String line = null; //строка из файла
         int i = 0;
         int j = 0;
 
+        BoyNames aBoys;
+        aBoys = new BoyNames(5);
+        aBoys.readNamesFromFile("D:\\eclipse\\newworkspace\\Hello\\bin\\com\\lux\\classes\\boynames.txt");
+
+        //System.out.println(Arrays.toString(aBoys));
+
+/*
         //try - this block
         try (
                 BufferedReader br = new BufferedReader(new FileReader("D:\\eclipse\\newworkspace\\Hello\\bin\\com\\lux\\classes\\boynames.txt"));
@@ -38,7 +46,7 @@ public class BoyNamesDemo {
         System.out.println(names[0]);
         System.out.println(names[1]);
 
-
+*/
         /*Scanner in = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream("hello_utf8.txt"), "UTF-8")));
         in.useDelimiter("[\\.\\s\\!]+");
         while (in.hasNext()) {
