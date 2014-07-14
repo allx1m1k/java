f71a955cd298d5270db3538fd4506585718b9610
Dima <dima@ua2012.org.ua>
2014-07-14 08:00:16 +0200

    nothing changes

--- Hello/src/com/lux/assignment/BoyNamesDemo.java
+++ Hello/src/com/lux/assignment/BoyNamesDemo.java
@@ -12,10 +12,10 @@ public class BoyNamesDemo {
 
     /**
      * метод ввода имени с клавиатуры
-     * @return
+     * @return экземпляр BoyName
      * @throws BoyNameNotFoundException
      */
-    public static BoyName inputBoyName () throws BoyNameNotFoundException {
+    public static String inputBoyName () throws BoyNameNotFoundException {
         //создадим объект потока ввода
         Scanner s = new Scanner(System.in);
         //System.out.println("Enter a number from 1 to 3:");
@@ -25,63 +25,33 @@ public class BoyNamesDemo {
         String aName = s.nextLine();
         //имя не было введено
         if (aName.equals("")) {
-            throw new BoyNameNotFoundException("SOS!");
+            throw new BoyNameNotFoundException("User has interrupted process!");
         }
         //создадим экземпляр BoyName с именем введенным с клавиатуры
-        BoyName newBoy = new BoyName(aName, "0");
-        return newBoy;
+        return aName;
 
     }
     public static void main(String[] args) throws BoyNameNotFoundException {
         Object[]names = new Object[5];
         String line = null; //строка из файла
-
-
-        BoyNameList aBoys;
+        BoyNameList aBoys; //список имен
         aBoys = new BoyNameList(5);
         aBoys.readNamesFromFile("D:\\eclipse\\newworkspace\\Hello\\bin\\com\\lux\\classes\\boynames.txt");
-
+        //выведем список aBoys
         System.out.println(aBoys.toPrint());
-
-        BoyName newBoyName = BoyNamesDemo.inputBoyName();
+        //запросим ввод имени
+        String newBoyName = BoyNamesDemo.inputBoyName();
+        //создадим экземпляр BoyName из введенной строки
+        BoyName newBoy = new BoyName(newBoyName, "0");
         System.out.println("");
-        System.out.println("New boy name has been entered by user is: " + newBoyName.getBoyName());
+        System.out.println("New boy name has been entered by user is: " + newBoy.getBoyName());
         System.out.println("");
-        System.out.println(aBoys.searchBoyName(newBoyName.getBoyName()));
 
-        int index = aBoys.searchBoyName(newBoyName.getBoyName());
+        //выполним поиск в списке aBoys введенного имени из экземпляра newBoy
+        System.out.println(aBoys.searchBoyName(newBoy.getBoyName()));
+
+        //для отладки выведем индекс искомого имени в списке
+        int index = aBoys.searchBoyName(newBoy.getBoyName());
         System.out.println("Freq of found name is: " + aBoys.getNameAt(index).getNameFreq());
-
-
-
-/*
-        //try - this block
-        try (
-                BufferedReader br = new BufferedReader(new FileReader("D:\\eclipse\\newworkspace\\Hello\\bin\\com\\lux\\classes\\boynames.txt"));
-
-        ) {
-            //String line;
-            //считываем построчно пока не достигли последней строки
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-                //names[i] = line.split("[ \t]");
-                names = line.split(" ");
-
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(Arrays.toString(names));
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-
-*/
-        /*Scanner in = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream("hello_utf8.txt"), "UTF-8")));
-        in.useDelimiter("[\\.\\s\\!]+");
-        while (in.hasNext()) {
-            System.out.println(in.next());
-        }
-    */
     }
-
 }
