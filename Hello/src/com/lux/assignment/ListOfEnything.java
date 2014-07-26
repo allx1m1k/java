499154189e7ef47238267974dc3f44ce9d6f85ab
Dima <dima@ua2012.org.ua>
2014-07-26 14:42:01 +0200

    Lab4 starts

---
+++ Hello/src/com/lux/assignment/ListOfEnything.java
@@ -0,0 +1,19 @@
+package com.lux.assignment;
+
+import java.util.AbstractList;
+
+/**
+ * Created by dima on 7/26/2014.
+ */
+public class ListOfEnything extends AbstractList {
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
