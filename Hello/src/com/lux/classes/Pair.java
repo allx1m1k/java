48da301fb12f51a8a454da8ec435de23c1a77333
Dima <dima@ua2012.org.ua>
2014-07-26 13:09:27 +0200

    Some clarifying comments

--- Hello/src/com/lux/classes/Pair.java
+++ Hello/src/com/lux/classes/Pair.java
@@ -14,9 +14,9 @@ public class Pair<K extends Number, V extends String>{
         }
 
     public static void main(String[] args) {
-        //Pair<Number, String> aPair = new Pair<>(1.00, "Abc"); тут получаем raw type т.к. при создании new Pair<> мы не указали типы в <>
-        Pair<Integer, String> aPair = new Pair<Integer, String>(1, "Abc"); //работает т.к. Integer является наследником от Number
-        Pair<Double, String> anotherPair = new Pair<>(10.0, "The A");
+        //Pair<Number, String> aPair = new Pair<>(1.00, "Abc"); ограничение - тут получаем raw type т.к. при создании new Pair<> мы не указали типы в <>
+        Pair<Number, String> aPair = new Pair<Number, String>(1, "Abc"); //работает т.к. Integer - 1 является наследником от Number
+        Pair<Number, String> anotherPair = new Pair<Number, String>(10.0, "The A"); //также работает т.к. Double - 10.0 является наследником от Number
 
     }
 
