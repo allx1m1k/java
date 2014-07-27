7b8bcafcd886506a34427e402183b6efddca8683
Dima <dima@ua2012.org.ua>
2014-07-27 20:58:01 +0200

    Lab4: small changes

--- Hello/src/com/lux/assignment/SortedArrayList.java
+++ Hello/src/com/lux/assignment/SortedArrayList.java
@@ -6,13 +6,21 @@ import java.util.AbstractList;
  * Created by dima on 7/26/2014.
  */
 public class SortedArrayList<T extends AbstractList > {
+    //в данной переменной храним структуру данных типа T
+    private T sortedList;
 
     /**
-     * Constructor
-     * @param anArray в качестве аршумента конструктора передаеи тип T - путукшс, т.е. тип как пераметр
+     * Default Constructor
      */
-    public SortedArrayList(T anArray) {
+    public SortedArrayList() {
 
+    }
+    /**
+     * Constructor
+     * @param anList в качестве аршумента конструктора передаеи тип T - путукшс, т.е. тип как пераметр
+     */
+    public SortedArrayList(T anList) {
+        this.sortedList = anList;
     }
 
 }
--- Hello/src/com/lux/assignment/SortedArrayListDemo.java
+++ Hello/src/com/lux/assignment/SortedArrayListDemo.java
@@ -1,5 +1,6 @@
 package com.lux.assignment;
 
+import java.util.AbstractList;
 import java.util.LinkedList;
 
 /**
@@ -8,6 +9,18 @@ import java.util.LinkedList;
 public class SortedArrayListDemo {
 
     public static void main(String[] args) {
+        /*
+         List<Student> students = new ArrayList<>(); //хорошее правило считается что объявлять надо на высшем уровне иерархии - работа с
+        //абстракцией чтобы можно было потом менять реализации
+        students.add(new Student(1, "Ivan", "Ivanov", new BigDecimal(100)));
+        students.add(new Student(2, "Petr", "Petrov", new BigDecimal(30)));
+        students.add(new Student(2, "Sidr", "Sidorov", new BigDecimal(30)));
+
+        for (Student s: students) {
+            System.out.println(s);
+        }
+         */
+
         SortedArrayList<LinkedList> aList = new SortedArrayList<LinkedList>();
     }
 }
