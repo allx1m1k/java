33eccc0a6d4b259f3dd823e6d61f2802c3703324
Dima <dima@ua2012.org.ua>
2014-07-31 19:27:52 +0200

    Add to .classpath JUnit

--- Hello/src/com/lux/assignment/LRUCacheDemo.java
+++ Hello/src/com/lux/assignment/LRUCacheDemo.java
@@ -8,10 +8,16 @@ public class LRUCacheDemo {
     public static void main(String[] args) {
         // Cache aCache = new LRUCacheImpl<>(5);  тут будет raw type
         Cache<Integer, String> aCache = new LRUCacheImpl<>(5); //можем не дублировать передачу параметров типа
-        aCache.put(1, "dddd");
+        aCache.put(1, "mmm1");
+        aCache.put(2, "mmm2");
+        aCache.put(3, "mmm3");
         aCache.get(1);
-        aCache.put(2, "dssssddd");
-        aCache.put(3, "ddfdgfssssddd");
+        aCache.get(3);
+        aCache.get(1);
+        aCache.put(4, "mmm4");
+        aCache.put(5, "mmm5");
+        aCache.put(6, "mmm6");
+        aCache.put(7, "mmm7");
 
         System.out.println(aCache.get(1));
         System.out.println(aCache.get(2));
