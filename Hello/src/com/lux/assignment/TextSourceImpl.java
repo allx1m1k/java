4db5e24797fce91e4f358bc5ff417059be7fc848
Dima <dima@ua2012.org.ua>
2014-08-02 16:24:29 +0200

    urls to regular express

--- Hello/src/com/lux/assignment/TextSourceImpl.java
+++ Hello/src/com/lux/assignment/TextSourceImpl.java
@@ -3,6 +3,9 @@ package com.lux.assignment;
 /**
  * Класс-реализация интерфейса Source
  * Моделирует в качестве источника текстовый файл
+ * http://www.javaportal.ru/java/articles/regexp.html
+ * http://www.javaprobooks.ru/java-%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/java-%D1%80%D0%B5%D0%B3%D1%83%D0%BB%D1%8F%D1%80%D0%BD%D1%8B%D0%B5-%D0%B2%D1%8B%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F.html
+ * http://pro-java.ru/java-dlya-nachinayushhix/regulyarnye-vyrazheniya-java/
  * Created by dima on 8/2/2014.
  */
 public class TextSourceImpl implements Source {
