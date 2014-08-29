8daf8769f72b26fd66d947373b9f0c2caa07e0cf
Dima <dima@ua2012.org.ua>
2014-08-30 01:19:42 +0200

    implemented: +eat(): void +run(): void

--- Hello/src/com/lux/assignment/class19/SyncPhilosopher.java
+++ Hello/src/com/lux/assignment/class19/SyncPhilosopher.java
@@ -1,5 +1,10 @@
 package com.lux.assignment.class19;
 
+import com.sun.corba.se.impl.orbutil.concurrent.Sync;
+
+import static java.lang.Thread.interrupted;
+import static java.lang.Thread.sleep;
+
 /**
  * Created by dima on 8/27/2014.
  */
@@ -7,10 +12,11 @@ public class SyncPhilosopher implements Runnable {
 
     private String name;
     private int number;
-    private Fork leftFork;
-    private Fork rightFork;
+    private SyncFork leftFork;
+    private SyncFork rightFork;
+    private int eatCount;
 
-    public SyncPhilosopher(String aName, int aNumber, Fork lFork, Fork rFork) {
+    public SyncPhilosopher(String aName, int aNumber, SyncFork lFork, SyncFork rFork) {
         this.name = aName;
         this.number = aNumber;
         //it will match Forks with the Philosopher, but don't block (lock) them
@@ -39,8 +45,29 @@ public class SyncPhilosopher implements Runnable {
         } else return this.number;
     }
 
+
+    public synchronized void eat(){
+        System.out.println("Philosopher N" + this.number + " " + this.name + " is eating!");
+        eatCount++;
+    }
+
     @Override
     public void run() {
+        //while Thread not interrupted
+        while (!interrupted()) {
+            synchronized (this.leftFork){
+                //this is the place to make a link between this particular Philosopher and evaluated left Fork
+                System.out.println("Left Fork" + this.leftFork.getNumber());
+                synchronized (this.rightFork) {
+                    eat();
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
 
     }
 }
