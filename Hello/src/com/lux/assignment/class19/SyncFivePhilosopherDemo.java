4f2dae605da3d23460bc1c98f12d02b52e418d8d
Dima <dima@ua2012.org.ua>
2014-08-31 00:31:45 +0200

    homepc

--- Hello/src/com/lux/assignment/class19/SyncFivePhilosopherDemo.java
+++ Hello/src/com/lux/assignment/class19/SyncFivePhilosopherDemo.java
@@ -7,7 +7,34 @@ public class SyncFivePhilosopherDemo {
 
     public static void main(String[] args) {
         System.out.println("Hello My Friend");
+
+        //arrays of Forks
+        SyncFork[] forks = new SyncFork[5];
+        for (int i = 0; i< 5; i++) {
+            forks[i] = new SyncFork(i);
+        }
+
+        //create philosopher
+        SyncPhilosopher philosopher0 = new SyncPhilosopher("Goraciy", 0, forks[0], forks[4]);
+        Thread p0Thread = new Thread(philosopher0, philosopher0.getName());
+
+        //create philosopher
+        SyncPhilosopher philosopher1 = new SyncPhilosopher("Newton", 1, forks[0], forks[1]);
+        Thread p1Thread = new Thread(philosopher1, philosopher1.getName());
+
+        p0Thread.start();
+        p1Thread.start();
+
+        try {
+            Thread.currentThread().sleep(5000);
+            p0Thread.interrupt();
+            p1Thread.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            //Thread.currentThread().interrupt();
+        }
+
+        System.out.println(philosopher0.getName() + " has eaten " + philosopher0.getEatCount() + " times");
+        System.out.println(philosopher1.getName() + " has eaten " + philosopher1.getEatCount() + " times");
     }
-
-
 }
--- Hello/src/com/lux/assignment/class19/SyncPhilosopher.java
+++ Hello/src/com/lux/assignment/class19/SyncPhilosopher.java
@@ -51,19 +51,31 @@ public class SyncPhilosopher implements Runnable {
         eatCount++;
     }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public synchronized int getEatCount() {
+        return this.eatCount;
+    }
+
     @Override
     public void run() {
         //while Thread not interrupted
         while (!interrupted()) {
             synchronized (this.leftFork){
+                System.out.println("Philosopher " + this.name + " takes left Fork N " + this.leftFork.getNumber());
                 //this is the place to make a link between this particular Philosopher and evaluated left Fork
-                System.out.println("Left Fork" + this.leftFork.getNumber());
                 synchronized (this.rightFork) {
+                    System.out.println("Philosopher " + this.name + " takes right Fork N " + this.rightFork.getNumber());
                     eat();
                     try {
-                        sleep(1000);
+                        Thread.currentThread().sleep(1000);
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        //e.printStackTrace();
+                        //this will safely interrupt the Thread
+                        //see http://stackoverflow.com/questions/1087475/when-does-javas-thread-sleep-throw-interruptedexception
+                        Thread.currentThread().interrupt();
                     }
                 }
             }
