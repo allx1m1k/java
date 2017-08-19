0eb709fe8d21fdae8fc8b88fd864744069b168d9
Dima <dima@ua2012.org.ua>
2017-08-19 12:52:44 +0300

    Prefix operations

---
+++ Hello/src/com/example/exercises/PrefixOperation.java
@@ -0,0 +1,17 @@
+package com.example.exercises;
+
+/**
+ * Created by dima on 8/19/2017.
+ */
+public class PrefixOperation {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        int k = 0;
+        k++;
+        for (int i = 10; k <= i; --i) {
+            System.out.println(i);
+        }
+    }
+}
