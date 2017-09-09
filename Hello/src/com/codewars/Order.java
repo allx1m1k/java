06131d942092f8be621e932bcaa5a4d84a4e2a6d
Dima <dima@ua2012.org.ua>
2017-09-09 23:39:05 +0300

    Codewars kata

--- Hello/src/com/codewars/Order.java
+++ Hello/src/com/codewars/Order.java
@@ -1,5 +1,8 @@
 package com.codewars;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Your task is to sort a given string. Each word in the String will contain a single number.
  * This number is the position the word should have in the result.
@@ -8,13 +11,31 @@ package com.codewars;
  * The words in the input String will only contain valid consecutive numbers.
  * For an input: "is2 Thi1s T4est 3a" the function should return "Thi1s is2 3a T4est"
  */
-public class Order {
+public class Order{
 
     //static String words = new String("");
     public static String order(String words) {
         //initialize result with capacity of words length
         StringBuilder result = new StringBuilder(words.length());
+        //initialize arrays of Strings which make up words as formal argument
         String[] parts = words.split(" ");
+
+        //sort parts
+        // https://docs.google.com/document/d/1WrOKUbW9CsRj-aLiMUPgHhBQw0DLguiVVaHJctlcjK0/edit
+        // https://ru.stackoverflow.com/questions/600385/arrays-sort-%D0%A1%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0-%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D0%B0-%D1%81%D1%82%D1%80%D0%BE%D0%BA-%D0%BF%D0%BE-%D0%B4%D0%BB%D0%B8%D0%BD%D0%B5
+        //Arrays.sort(parts);
+
+        Arrays.sort(parts, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int c = Integer.compare(o1.length(), o2.length());
+                if (c == 0) {
+                    return o1.compareTo(o2);
+                } else {
+                    return c;
+                }
+            }
+        });
 
         // if formal argument is empty
         if (words.isEmpty())
@@ -30,4 +51,6 @@ public class Order {
         }
         return result.toString();
     }
+
+
 }
--- Hello/src/com/codewars/OrderDemo.java
+++ Hello/src/com/codewars/OrderDemo.java
@@ -1,11 +1,14 @@
 package com.codewars;
 
+import java.util.Comparator;
+
 /**
  * Created by dima on 9/9/2017.
  */
 public class OrderDemo {
     public static void main(String[] args) {
-        System.out.println(Order.order("is2 Thi1s T4est 3a"));
+        String s = Order.order("is2 123456 Thi1s T4est 1234567 3a 1 0");
+        System.out.println(s);
     }
 
 }
