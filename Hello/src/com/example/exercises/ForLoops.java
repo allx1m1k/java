a07841159929f9f9f5ac2f7211aeac568f265f3e
Dima <dima@ua2012.org.ua>
2017-09-18 23:07:45 +0300

    For loops doesn't work

---
+++ Hello/src/com/example/exercises/ForLoops.java
@@ -0,0 +1,16 @@
+package com.example.exercises;
+
+/**
+ * Created by dima on 9/18/2017.
+ */
+public class ForLoops {
+
+    public static void main(String[] args) {
+        //int i =0;
+        for (int i = 0, i <= 2, i++)
+            System.out.println(i);
+        for ( i = 0,  i <= 2, --i)
+            System.out.println(i);
+
+        }
+    }
