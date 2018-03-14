18c155516841e4c6b093ee5cbff14a6228a4c349
allx1m1k <UkraineChampion2012>
2018-03-15 00:24:30 +0200

    add linux path

--- Hello/src/com/lux/classes/class10/Class10.java
+++ Hello/src/com/lux/classes/class10/Class10.java
@@ -1,9 +1,8 @@
 package com.lux.classes.class10;
 
-import com.sun.corba.se.spi.orbutil.fsm.Input;
+//https://examples.javacodegeeks.com/java-basics/exceptions/java-io-filenotfoundexception-how-to-solve-file-not-found-exception/
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -13,7 +12,7 @@ import java.io.InputStream;
 public class Class10 {
 
     public static void main(String[] args) throws IOException {
-        InputStream is = new FileInputStream("D:\\WORK\\JAVA\\test.txt");
+        InputStream is = new FileInputStream("./test.txt");
 
         int val =  is.read();
         while (val != -1) {
