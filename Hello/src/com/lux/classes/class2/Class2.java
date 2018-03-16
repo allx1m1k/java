67cdb2b549ce9ef0fc2d3ae2aa4625e3b1230f66
allx1m1k <UkraineChampion2012>
2018-03-16 23:59:22 +0200

    utf8 ubuntu

--- Hello/src/com/lux/classes/class2/Class2.java
+++ Hello/src/com/lux/classes/class2/Class2.java
@@ -6,31 +6,31 @@ public class Class2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	int[] arrayOfIntegers; //объявление переменной без указания кол-ва
-	
-	arrayOfIntegers = new int[7]; //создание, выделение памяти создание все эелементы нулевые изменить размер нельзя
-	//изменять размеры массива можно только в коллекциях - динамические массывы
-	
-	int[] newArrayOfIntegers = new int[7];
-	
-	arrayOfIntegers[0] = 8; //заполнение значениями
-	arrayOfIntegers[1] = 11;
-	
-/*	double[] numbers = new double[5]; //объявления и инициализация/создание
-	for (int i = 0; i < 5 */
-	
-	int [] values = {1, 33, -6, 0, 22, 66, 8};
-	
-	System.arraycopy (values, 2, arrayOfIntegers, 3, 4); //копируем четыре элемента массива values в массив arrayOfIntegers
-	System.arraycopy (values, 2, newArrayOfIntegers, 0, 4); //аргументы Object src, srcPos, Object dest, destPos, length  
-	
-	System.out.println(Arrays.toString(arrayOfIntegers)); //
-	System.out.println("newArrayOfIntegers " + Arrays.toString(newArrayOfIntegers)); //
-	
-	Arrays.sort(values);
-	
-	System.out.println(Arrays.toString(values));
-	
-	
+		int[] arrayOfIntegers; //РѕР±СЉСЏРІР»РµРЅРёРµ РїРµСЂРµРјРµРЅРЅРѕР№ Р±РµР· СѓРєР°Р·Р°РЅРёСЏ РєРѕР»РёС‡РµСЃС‚РІР°
+
+		arrayOfIntegers = new int[7]; //СЃРѕР·РґР°РЅРёРµ, РІС‹РґРµР»РµРЅРёРµ РїР°РјСЏС‚Рё СЃРѕР·РґР°РЅРёРµ РІСЃРµ СЌРµР»РµРјРµРЅС‚С‹ РЅСѓР»РµРІС‹Рµ РёР·РјРµРЅРёС‚СЊ СЂР°Р·РјРµСЂ РЅРµР»СЊР·СЏ
+		//РёР·РјРµРЅСЏС‚СЊ СЂР°Р·РјРµСЂС‹ РјР°СЃСЃРёРІР° РјРѕР¶РЅРѕ С‚РѕР»СЊРєРѕ РІ РєРѕР»Р»РµРєС†РёСЏС… - РґРёРЅР°РјРёС‡РµСЃРєРёРµ РјР°СЃСЃС‹РІС‹
+
+		int[] newArrayOfIntegers = new int[7];
+
+		arrayOfIntegers[0] = 8; //Р·Р°РїРѕР»РЅРµРЅРёРµ Р·Р°РЅС‡РµРЅРёСЏРјРё
+		arrayOfIntegers[1] = 11;
+
+	/*	double[] numbers = new double[5]; // РѕР±СЉСЏРІР»РµРЅРёРµ Рё РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ/СЃРѕР·РґР°РЅРёРµ
+		for (int i = 0; i < 5 */
+
+		int [] values = {1, 33, -6, 0, 22, 66, 8};
+
+		System.arraycopy (values, 2, arrayOfIntegers, 3, 4); //РєРѕРїРёСЂСѓРµРј 4 СЌР»РµРјРµРЅС‚Р° РјР°СЃСЃРёРІР° values РІ РјР°СЃСЃРёРІ arrayOfIntegers
+		System.arraycopy (values, 2, newArrayOfIntegers, 0, 4); //Р°СЂРіСѓРјРµРЅС‚С‹ Object src, srcPos, Object dest, destPos, length
+
+		System.out.println(Arrays.toString(arrayOfIntegers)); //
+		System.out.println("newArrayOfIntegers " + Arrays.toString(newArrayOfIntegers)); //
+
+		Arrays.sort(values);
+
+		System.out.println(Arrays.toString(values));
+
+
 	}
 }
